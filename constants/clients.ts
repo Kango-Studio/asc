07f6ae2e10412f99119ce563d2c25ba6280d9c21
Mateusp23 <mateusp23@rede.ulbra.br>
2026-01-29 10:43:05 -0300

    aumentar logo clientes

--- constants/clients.ts
+++ constants/clients.ts
@@ -1,92 +1,98 @@
 export const clients = [
-    {
-      name: "Leffa Móveis",
-      segment: "Mobiliário e Decoração",
-      logo: "/clients/leffa-curto.jpg",
-    },
-    {
-      name: "Apara Barro Dema",
-      segment: "Indústria Cerâmica",
-      logo: "/clients/dema-2.png",
-    },
-    {
-      name: "Bananas Borges",
-      segment: "Distribuição e Comércio de Alimentos",
-      logo: "/clients/borges-bananas.png",
-    },
-    {
-      name: "Niel Diesel",
-      segment: "Autopeças e Serviços Automotivos",
-      logo: "/clients/nildiesel.png",
-    },
-    {
-      name: "Traffic Control",
-      segment: "Soluções em Tecnologia e Serviços",
-      logo: "/clients/traffic-control.png",
-    },
-    {
-      name: "Elimodas",
-      segment: "Moda e Vestuário",
-      logo: "/clients/elimodas.jpg",
-    },
-    {
-      name: "Otica Especialista",
-      segment: "Óptica e Oftalmologia",
-      logo: "/clients/otica-especialista.png",
+  {
+    name: "Leffa Móveis",
+    segment: "Mobiliário e Decoração",
+    logo: "/clients/leffa-curto.jpg",
+    isLargeSize: true,
   },
-    {
-      name: "Loja drop",
-      segment: "E-commerce e Dropshipping",
-      logo: "/clients/loja-drop.png",
+  {
+    name: "Apara Barro Dema",
+    segment: "Indústria Cerâmica",
+    logo: "/clients/dema-2.png",
   },
-    {
-      name: "Premier Imóveis",
-      segment: "Consultoria Imobiliária",
-      logo: "/clients/premier.jpg",
+  {
+    name: "Bananas Borges",
+    segment: "Distribuição e Comércio de Alimentos",
+    logo: "/clients/borges-bananas.png",
   },
-    {
-      name: "Vitória Veículos",
-      segment: "Comércio de Veículos",
-      logo: "/clients/vitoria-veiculos.png",
+  {
+    name: "Niel Diesel",
+    segment: "Autopeças e Serviços Automotivos",
+    logo: "/clients/nildiesel.png",
+    isLargeSize: true,
   },
-    {
-      name: "Cadô",
-      segment: "Marketing Digital e Comunicação",
-      logo: "/clients/cado.png",
+  {
+    name: "Traffic Control",
+    segment: "Soluções em Tecnologia e Serviços",
+    logo: "/clients/traffic-control.png",
+    isLargeSize: true,
   },
-    {
-      name: "Model",
-      segment: "Madeireira e Construção",
-      logo: "/clients/model.png",
+  {
+    name: "Elimodas",
+    segment: "Moda e Vestuário",
+    logo: "/clients/elimodas.jpg",
   },
-    {
-      name: "Total Tec",
-      segment: "Distribuição de Lubrificantes",
-      logo: "/clients/totaltec.png",
+  {
+    name: "Otica Especialista",
+    segment: "Óptica e Oftalmologia",
+    logo: "/clients/otica-especialista.png",
   },
-    {
-      name: "Ictel",
-      segment: "Telecomunicações e Tecnologia",
-      logo: "/clients/ictel.png",
+  {
+    name: "Loja drop",
+    segment: "E-commerce e Dropshipping",
+    logo: "/clients/loja-drop.png",
+    isLargeSize: true,
   },
-    {
-      name: "Nostra Net",
-      segment: "Serviços de Telecomunicações",
-      logo: "/clients/nostranet.jpg",
+  {
+    name: "Premier Imóveis",
+    segment: "Consultoria Imobiliária",
+    logo: "/clients/premier.jpg",
+    isLargeSize: true,
   },
-    {
-      name: "Harduston",
-      segment: "Construção Civil e Incorporação",
-      logo: "/clients/harduston.jpg",
+  {
+    name: "Vitória Veículos",
+    segment: "Comércio de Veículos",
+    logo: "/clients/vitoria-veiculos.png",
   },
-    {
-      name: "Pet Djor",
-      segment: "Pet Shop e Estética Animal",
-      logo: "/clients/pet-djor.jpg",
+  {
+    name: "Cadô",
+    segment: "Marketing Digital e Comunicação",
+    logo: "/clients/cado.png",
   },
-    {
-      name: "Hansen",
-      segment: "Advocacia e Consultoria Jurídica",
-      logo: "/clients/hansen-vertical.png",
-    },
-  ];
+  {
+    name: "Model",
+    segment: "Madeireira e Construção",
+    logo: "/clients/model.png",
+    isLargeSize: true,
+  },
+  {
+    name: "Total Tec",
+    segment: "Distribuição de Lubrificantes",
+    logo: "/clients/totaltec.png",
+  },
+  {
+    name: "Ictel",
+    segment: "Telecomunicações e Tecnologia",
+    logo: "/clients/ictel.png",
+  },
+  {
+    name: "Nostra Net",
+    segment: "Serviços de Telecomunicações",
+    logo: "/clients/nostranet.jpg",
+  },
+  {
+    name: "Harduston",
+    segment: "Construção Civil e Incorporação",
+    logo: "/clients/harduston.jpg",
+  },
+  {
+    name: "Pet Djor",
+    segment: "Pet Shop e Estética Animal",
+    logo: "/clients/pet-djor.jpg",
+  },
+  {
+    name: "Hansen",
+    segment: "Advocacia e Consultoria Jurídica",
+    logo: "/clients/hansen-vertical.png",
+  },
+];
